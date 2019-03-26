f13d070f83e09ed87923c58cb34b3d29c467aa89
quonn-bernard <qber83@gmail.com>
2019-03-26 09:54:55 -0700

    created NoteList Component

---
+++ noteful/src/NoteList/NoteList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+ function NoteList  (props) {  
+  console.log(props);  
+  return (
+      <div></div>
+    )
+    
+    }
+
+export default NoteList;
