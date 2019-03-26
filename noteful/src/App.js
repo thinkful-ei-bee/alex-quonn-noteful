c6447351a4a9ba2f99376b82dd8f7920193b9d9a
quonn-bernard <qber83@gmail.com>
2019-03-26 11:55:19 -0700

    Added comp routes and added NotesDesc component

--- noteful/src/App.js
+++ noteful/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import {Note} from "./Note/Note";
+import NoteDesc from "./NoteDesc/NoteDesc";
 import NoteList from "./NoteList/NoteList";
 import { Link } from 'react-router-dom';
 import {Route} from 'react-router-dom';
@@ -25,7 +25,6 @@ class App extends Component {
           <Link to="/">
           <h1>Noteful</h1>
           </Link>
-        })
       }
         </header>
         <aside>
@@ -39,16 +38,23 @@ class App extends Component {
           </nav>
         </aside>
       <main>
-      <Route exact path='/folder/folder.id'
-      render={(routeProps) => {
-        console.log("x")
-        const listNotes = this.state.notes.filter(note => note.id === routeProps.folder.id);
-         return (<NoteList>
-          notes = {listNotes};
-        </NoteList>);
-      }} />
-                      
-  
+
+      <Route exact path='/folder/:folderId'
+        component={(props) => {
+        const listNotes = this.state.notes.filter(note => note.folderId === props.match.params.folderId)
+        return <NoteList notes={listNotes}></NoteList>
+          }} 
+      />
+
+      <Route exact path='/note/:noteId'
+                   component={(props) => {
+                    const description = this.state.notes.filter(note => note.id === props.match.params.noteId)
+                   return <NoteDesc note={description}>
+                    
+                   </NoteDesc>
+                    }} 
+      />
+
       </main>
       </div>
     );
