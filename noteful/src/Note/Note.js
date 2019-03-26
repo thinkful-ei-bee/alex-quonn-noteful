eaadbffbbf8c050211a60c30e48f9f29c05d2d19
quonn-bernard <qber83@gmail.com>
2019-03-26 11:54:45 -0700

    Created indiviual notes

--- noteful/src/Note/Note.js
+++ noteful/src/Note/Note.js
@@ -3,15 +3,14 @@ import { Link } from 'react-router-dom';
 class Note extends Component {
   
   render() {
+    
     return (
         <div>
-            <header>
-          <Link to="/" >
-          <h1>Noteful</h1>
-          </Link>
-        </header>
-            <button></button>
-            <p></p>
+            <h1>
+            <Link to={'/note/'+ this.props.id}>  {this.props.name}  </Link> 
+            </h1>
+            <button>Delete</button>
+            <p>{this.props.date}</p>
         </div>
     )
   }
