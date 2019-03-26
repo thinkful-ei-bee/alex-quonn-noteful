24544f160804e183062750318013eee16de40b5c
quonn-bernard <qber83@gmail.com>
2019-03-26 08:11:41 -0700

    Added a header containing home link

--- noteful/src/App.js
+++ noteful/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          
+        <header>
+          <Link to="/" >
+          <h1>Noteful</h1>
+          </Link>
         </header>
       </div>
     );
